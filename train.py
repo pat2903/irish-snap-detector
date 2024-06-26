9761719255c2c1d7192f54f58ce85d7ff4f7f6f6
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-06-26 16:47:53 +0700

    create train.py

---
+++ train.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+# paths
+train_dir = 'dataset/train'
+test_dir = 'dataset/test'
+validate_dir = 'dataset/valid'
